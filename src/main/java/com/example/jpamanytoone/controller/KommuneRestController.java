3f2c0d74911946ccf2b7f3cd311bc84e41770c85
kjartanleander <Kjar0321@stud.kea.dk>
2023-09-04 18:32:41 +0200

    Tilføjet put, post, delete endpoints i region og kommune

--- src/main/java/com/example/jpamanytoone/controller/KommuneRestController.java
+++ src/main/java/com/example/jpamanytoone/controller/KommuneRestController.java
@@ -2,11 +2,12 @@ package com.example.jpamanytoone.controller;
 
 import com.example.jpamanytoone.model.Kommune;
 import com.example.jpamanytoone.model.Region;
+import com.example.jpamanytoone.repository.KommuneRepository;
 import com.example.jpamanytoone.service.ApiServiceGetKommuner;
 import com.example.jpamanytoone.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -15,10 +16,31 @@ public class KommuneRestController {
     @Autowired
     ApiServiceGetKommuner apiServiceGetKommuner;
 
+    @Autowired
+    KommuneRepository kommuneRepository;
+
     @GetMapping("/getkommuner")
     public List<Kommune>getKommuner() {
         List<Kommune> lstKommuner = apiServiceGetKommuner.getKommuner();
         return lstKommuner;
+    }
+    @PutMapping("/kommuner")
+    public Kommune putKommune(@RequestBody Kommune kommune){
+        System.out.println(kommune);
+        return kommuneRepository.save(kommune);
+    }
+    @PostMapping("/kommuner")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Kommune postKommune(@RequestBody Kommune kommune){
+        System.out.println(kommune);
+        return kommuneRepository.save(kommune);
+    }
+
+    @DeleteMapping("/kommune")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteKommune(@RequestBody Kommune kommune){
+        kommuneRepository.delete(kommune);
+        System.out.println(kommune + " Deleted");
     }
 }
 
--- src/main/java/com/example/jpamanytoone/controller/RegionRestController.java
+++ src/main/java/com/example/jpamanytoone/controller/RegionRestController.java
@@ -1,10 +1,13 @@
 package com.example.jpamanytoone.controller;
 
+import com.example.jpamanytoone.model.Kommune;
 import com.example.jpamanytoone.model.Region;
+import com.example.jpamanytoone.repository.KommuneRepository;
+import com.example.jpamanytoone.repository.RegionRepository;
 import com.example.jpamanytoone.service.ApiServiceGetRegioner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,9 +17,31 @@ public class RegionRestController {
     @Autowired
     ApiServiceGetRegioner apiServiceGetRegioner;
 
+    @Autowired
+    RegionRepository regionRepository;
+
     @GetMapping("/getregioner")
     public List<Region> getRegioner() {
         List<Region> lstRegioner = apiServiceGetRegioner.getRegioner();
         return lstRegioner;
+    }
+
+    @PutMapping("/regioner")
+    public Region putRegion(@RequestBody Region region){
+        System.out.println(region);
+        return regionRepository.save(region);
+    }
+    @PostMapping("/regioner")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Region postRegion(@RequestBody Region region){
+        System.out.println(region);
+        return regionRepository.save(region);
+    }
+
+    @DeleteMapping("/region")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteRegion(@RequestBody Region region){
+        regionRepository.delete(region);
+        System.out.println(region + " Deleted");
     }
 }
